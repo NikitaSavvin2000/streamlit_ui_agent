5713debc7d8467867fcf423a002dcc625cece52c
Nikita <savvin.nikit@yandex.ru>
2025-06-19 17:05:24 +0300

    save

--- Dockerfile
+++ Dockerfile
@@ -1,22 +1,20 @@
-FROM python:3.10.1-buster
+FROM python:3.11-buster
 USER root
 RUN apt-get update
-RUN apt-get install -y vim
+RUN apt-get install -y vim poppler-utils
 
-WORKDIR app
+WORKDIR /app
 COPY ./ ./
 
 COPY pyproject.toml pdm.lock README.md ./
-RUN apt-get update && apt-get install -y poppler-utils
 
 RUN pip install -U pip setuptools wheel
+RUN pip install zstandard
 RUN pip install pdm
-RUN pdm install --prod --no-lock --no-editable
+RUN pdm install --prod --frozen-lockfile --no-editable
 RUN pdm build
 RUN pdm install
-
 
 EXPOSE 8501
 
 ENTRYPOINT ["pdm", "run", "src/server.py"]
-
--- src/ui/pages/main_page.py
+++ src/ui/pages/main_page.py
@@ -1,29 +1,55 @@
-import asyncio
 import streamlit as st
+import pandas as pd
+import time
 
-from ui.utils.greating import greatings
+st.set_page_config(page_title="Horizon AI", layout="wide")
 
-from ui.utils.data_processing import check_authentication
+if "chat_history" not in st.session_state:
+    st.session_state.chat_history = []
 
+def bot_response_stream():
+    reply = "👋 Привет! Я — ИИ агент **Horizon** 🤖. Сейчас я нахожусь в разработке 🛠️. В будущем я смогу:📈 строить прогнозы временных рядов, 📊 анализировать данные, 📉 рисовать графики, 📋 и работать с таблицами, Оставайтесь на связи — скоро будет мощно! 🚀"
+    for char in reply:
+        yield char
+        time.sleep(0.01)
 
-@check_authentication
-async def main():
-    st.sidebar.write('# Здесь будут всякие параметры')
+def main():
+    with st.sidebar:
+        st.button("Новый чат", use_container_width=True)
+        st.title("💬 Чаты")
+        st.button("Чат 1", use_container_width=True)
+        st.button("Чат 2", use_container_width=True)
+        st.button("Чат 3", use_container_width=True)
 
-    if st.button('Скажи привет!'):
-        greatings()
-        st.sidebar.write('## Привет Саша')
+    _, central, _ = st.columns([1, 3, 1])
+    central.title("🧠 Horizon AI")
+    messages = central.container(height=500, border=False)
 
+    prompt = central.chat_input("Say something and/or attach an image", accept_file=True, file_type=["csv", "xlsx"])
+
+    if prompt:
+        if prompt.text:
+            st.session_state.chat_history.append(("user", prompt.text))
+            st.session_state.chat_history.append(("assistant_stream", None))
+
+        if prompt.files:
+            file = prompt.files[0]
+            if file.name.endswith(".csv"):
+                df = pd.read_csv(file)
+            elif file.name.endswith(".xlsx"):
+                df = pd.read_excel(file)
+            else:
+                df = None
+
+            if df is not None:
+                st.session_state.chat_history.append(("user", f"Файл: {file.name}"))
+                st.session_state.chat_history.append(("assistant_stream", None))
+
+    for msg_type, msg_content in st.session_state.chat_history:
+        if msg_type == "assistant_stream":
+            messages.chat_message("assistant").write_stream(bot_response_stream)
+        else:
+            messages.chat_message(msg_type).write(msg_content)
 
 if __name__ == "__main__":
-    loop = asyncio.new_event_loop()
-    asyncio.set_event_loop(loop)
-    try:
-        asyncio.run(main())
-    except KeyboardInterrupt:
-        pass
-    finally:
-        loop.close()
-
-
-
+    main()
