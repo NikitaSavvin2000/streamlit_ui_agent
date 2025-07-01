8b13108f000f1342adeb0f1656b3e51dac3ad461
Nikita <savvin.nikit@yandex.ru>
2025-07-01 12:36:45 +0300

    feat: new realize

--- src/ui/pages/main_page.py
+++ src/ui/pages/main_page.py
@@ -1,55 +1,153 @@
 import streamlit as st
+
+st.set_page_config(page_title="Horizon AI", layout="wide")
+
+import streamlit as st
+import requests
+import uuid
 import pandas as pd
-import time
+import streamlit.components.v1 as components
+import random
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+
+cwd = os.getcwd()
+path_to_avatar = os.path.join(cwd, "src", "ui", "logos", "single_logo.png")
+path_to_logo = os.path.join(cwd, "src", "ui", "logos", "short_logo_black.png")
+
+
+
 
 st.set_page_config(page_title="Horizon AI", layout="wide")
 
 if "chat_history" not in st.session_state:
     st.session_state.chat_history = []
+if "session_id" not in st.session_state:
+    st.session_state.session_id = str(uuid.uuid4())
+if "chat_id" not in st.session_state:
+    st.session_state.chat_id = None
 
-def bot_response_stream():
-    reply = "👋 Привет! Я — ИИ агент **Horizon** 🤖. Сейчас я нахожусь в разработке 🛠️. В будущем я смогу:📈 строить прогнозы временных рядов, 📊 анализировать данные, 📉 рисовать графики, 📋 и работать с таблицами, Оставайтесь на связи — скоро будет мощно! 🚀"
-    for char in reply:
-        yield char
-        time.sleep(0.01)
+
+def send_to_backend(session_id: str, chat_id: str, message: str = "", filename: str | None = None, data_json: list | None = None):
+    base_url = os.getenv("CHAT_API", "http://0.0.0.0:7070")
+    url = f"{base_url}/chat"
+    payload = {
+        "session_id": session_id,
+        "chat_id": chat_id,
+        "message": message,
+        "filename": filename,
+        "data_json": data_json,
+    }
+    response = requests.post(url, json=payload)
+    if response.status_code == 200:
+        res_json = response.json()["response"]
+        return res_json
+    return {"message": "Ошибка при запросе к бэкенду"}
+
+
 
 def main():
-    with st.sidebar:
-        st.button("Новый чат", use_container_width=True)
-        st.title("💬 Чаты")
-        st.button("Чат 1", use_container_width=True)
-        st.button("Чат 2", use_container_width=True)
-        st.button("Чат 3", use_container_width=True)
+    st.sidebar.image(path_to_logo)
 
-    _, central, _ = st.columns([1, 3, 1])
-    central.title("🧠 Horizon AI")
-    messages = central.container(height=500, border=False)
+    # st.sidebar.markdown(
+    #     """
+    # 🚧 **Horizon AI сейчас в режиме отладки.**
+    #
+    # ---
+    #
+    # 🔜 Скоро он сможет:
+    #
+    #
+    # 📊 Работать с таблицами CSV и XLSX
+    #
+    #
+    # 📈 Прогнозировать временные ряды
+    #
+    #
+    # 🧠 Анализировать данные
+    #
+    #
+    # 📌 Рисовать информативные графики
+    #
+    #
+    # 🪄 Интерактивно работать в чате
+    # """
+    # )
 
-    prompt = central.chat_input("Say something and/or attach an image", accept_file=True, file_type=["csv", "xlsx"])
+    st.sidebar.markdown(
+        """
+    **Horizon AI сейчас работает в режиме отладки.**
+    
+    ---
+    
+    Скоро он сможет:
+    
+    - Работать с таблицами CSV и XLSX  
+    
+    - Прогнозировать временные ряды  
+    
+    - Анализировать данные  
+    
+    - Рисовать информативные графики  
+    
+    - Обеспечивать интерактивное взаимодействие в чате  
+    """
+    )
+    messages = st.container(height=650, border=False)
 
-    if prompt:
-        if prompt.text:
-            st.session_state.chat_history.append(("user", prompt.text))
-            st.session_state.chat_history.append(("assistant_stream", None))
+    if "chat_history" not in st.session_state:
+        st.session_state.chat_history = []
+    if "pending_input" not in st.session_state:
+        st.session_state.pending_input = None
 
-        if prompt.files:
-            file = prompt.files[0]
-            if file.name.endswith(".csv"):
-                df = pd.read_csv(file)
-            elif file.name.endswith(".xlsx"):
-                df = pd.read_excel(file)
-            else:
-                df = None
+    emojis = ["🗿", "🚀", "🛸", "🏂", "🤺",]
+    emoji = random.choice(emojis)
 
-            if df is not None:
-                st.session_state.chat_history.append(("user", f"Файл: {file.name}"))
-                st.session_state.chat_history.append(("assistant_stream", None))
+    prompt = st.chat_input("Say something")
 
-    for msg_type, msg_content in st.session_state.chat_history:
-        if msg_type == "assistant_stream":
-            messages.chat_message("assistant").write_stream(bot_response_stream)
-        else:
-            messages.chat_message(msg_type).write(msg_content)
+    if prompt and not st.session_state.pending_input:
+        st.session_state.pending_input = prompt
+
+    with messages:
+        for role, content, df_data, html in st.session_state.chat_history:
+            avatar = path_to_avatar if role == "assistant" else None
+            with st.chat_message(role, avatar=avatar):
+                st.write(content)
+                if df_data:
+                    st.dataframe(pd.DataFrame(df_data))
+                if html:
+                    components.html(html, height=500, scrolling=False)
+
+        # отрисовать новое сообщение пользователя + спиннер
+        if st.session_state.pending_input:
+            with st.chat_message("user"):
+                st.write(st.session_state.pending_input)
+
+            with st.chat_message("assistant", avatar=path_to_avatar):
+                # with st.spinner(f"{emoji} Horizon думает..."):
+                with st.spinner(' '):
+
+                    session_id = '1234'
+                    chat_id = '2345'
+                    user_input = st.session_state.pending_input
+                    reply = send_to_backend(
+                        session_id=session_id,
+                        chat_id=chat_id,
+                        message=user_input,
+                        filename=None,
+                        data_json=None
+                    )
+                    st.session_state.chat_history.append(("user", user_input, None, None))
+                    st.session_state.chat_history.append((
+                        "assistant",
+                        reply["message"],
+                        reply.get("data"),
+                        reply.get("chart_html")
+                    ))
+                    st.session_state.pending_input = None
+                    st.rerun()
 
 if __name__ == "__main__":
     main()
